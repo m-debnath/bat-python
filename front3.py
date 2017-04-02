f0b7b552341ced58477449caa227c92b7368128d
Mukul Debnath <Mukul Debnath>
2017-04-02 23:55:18 +0530

    front3

---
+++ front3.py
@@ -0,0 +1,8 @@
+def front3(str):
+    """Given a string, we'll say that the front is the first 3 chars of the string.
+    
+    If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front."""
+    if len(str) < 3:
+        return str*3
+    else:
+        return str[:3]*3
