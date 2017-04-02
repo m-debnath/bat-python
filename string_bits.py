3518841ccfcbb752b697118b7e74e5ef926b2ea4
Mukul Debnath <Mukul Debnath>
2017-04-03 01:52:30 +0530

    string_bits

--- string_bits.py
+++ string_bits.py
@@ -1,4 +1,6 @@
 def string_bits(str):
+    """Given a string, return a new string made of every other char starting
+    with the first, so "Hello" yields "Hlo"."""
     result = ""
     if len(str) > 1:
         for i in range(len(str)):
